03a13bf2befdc9ea194a5281c7ce4f48f1ad3b7c
FredHSQ <frederico.shere@gmail.com>
2022-08-04 15:17:07 -0300

    Aula 1

---
+++ SerraMusic/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+function App () {
+  return <View>
+    <Text>
+      Olá Serratec
+    </Text>
+  </View>
+}
+
+export default App
