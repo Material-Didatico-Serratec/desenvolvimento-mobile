f0f5b4314e89c07bd01a3bfd4d9a6793091e422c
FredHSQ <frederico.shere@gmail.com>
2022-08-23 13:38:42 -0300

    Aula 10

---
+++ Aula10/SerraMusic/src/components/ModalLetra/index.tsx
@@ -0,0 +1,117 @@
+import React, { useContext, useEffect, useState } from "react";
+import { Image, Modal, Text, TouchableOpacity, View } from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
+import { buscaLetra } from "../../services/api";
+import { styles } from "./styles";
+
+import IconGoBack from '../../assets/icons/goback.png';
+import EstrelaVazia  from "../../assets/icons/estrelaVazia.png";
+import EstrelaCheia  from "../../assets/icons/estrelaCheia.png";
+import { ContextoFavorito } from "../../context/favoritos";
+
+interface ModalLetraProps {
+    visibilidadeModal: boolean,
+    idAlbum: number,
+    idMusica: number,
+    setVisibilidadeModal: React.Dispatch<React.SetStateAction<boolean>>,
+}
+
+interface LetraProps {
+    id: number,
+    letra: string,
+    titulo: string,
+    minutos: number
+}
+
+export const ModalLetra = ({ visibilidadeModal, idAlbum, idMusica, setVisibilidadeModal }: ModalLetraProps) => {
+
+    const [letra, setLetra] = useState<LetraProps>();
+    const [favorito, setFavorito] = useState<boolean>(false);
+
+    const { 
+        adicionaItemFavoritos, 
+        listaFavorita, 
+        retiraItemFavoritos 
+    } =  useContext(ContextoFavorito)
+
+    useEffect(() => {
+        buscaLetra(idAlbum, idMusica).then((res) => {
+            setLetra(res.data);
+        }).catch((err) => {
+            console.log(err)
+        });
+        listaFavorita.map(musica=>{
+            if(musica.id === idMusica) {
+                setFavorito(true);
+            }
+        });
+    }, [])
+
+    function lidaFavorito() {
+        if(!favorito) {
+            if (!listaFavorita.find((musica)=>{
+                if (musica.id === idMusica) {
+                    return true
+                } else {
+                    return false
+                }
+            })) {
+                letra  && adicionaItemFavoritos({id: idMusica, titulo: letra.titulo, minutos: letra.minutos})
+                setFavorito(true);
+            }
+        } else {
+            listaFavorita.find((musica)=>{
+                if(musica.id === idMusica) {
+                    retiraItemFavoritos(idMusica);
+                    setFavorito(false);
+                }
+            })
+        }
+    }
+
+    return <Modal
+        animationType="slide"
+        visible={visibilidadeModal}
+        transparent={true}
+        onRequestClose={() => {
+            setVisibilidadeModal(false);
+        }}
+    >
+        <View style={styles.modal}>
+            {letra ? <View style={styles.container}>
+                <View style={styles.tituloContainer}>
+                    <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                        <TouchableOpacity onPress={() => setVisibilidadeModal(false)}>
+                            <Image
+                                source={IconGoBack}
+                                style={{ width: 15, height: 15 }}
+                            />
+                        </TouchableOpacity>
+                        <Text style={styles.titulo}>
+                            {letra.titulo}
+                        </Text>
+                    </View>
+                    <Text style={styles.duracao}>
+                        {letra.minutos / 60 > 1 && Math.floor(letra.minutos / 60) + "h "}{letra.minutos % 60 + "min"}
+                    </Text>
+                </View>
+                <TouchableOpacity onPress={lidaFavorito} style={styles.favoritoContainer}>
+                    <Text>
+                        Favoritar:
+                    </Text>
+                    <Image source={favorito ? EstrelaCheia: EstrelaVazia} style={styles.image}/>
+                </TouchableOpacity>
+                <ScrollView style={{ paddingHorizontal: 5 }}>
+                    <Text>
+                        {letra.letra}
+                    </Text>
+                </ScrollView>
+            </View>
+                :
+                <Text>
+                    Carregando...
+                </Text>
+            }
+        </View>
+    </Modal>
+}
---
+++ Aula10/SerraMusic/src/screens/Musicas/index.tsx
@@ -0,0 +1,86 @@
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import React, { useEffect, useState } from "react";
+import { Image, Text, TouchableOpacity, View, } from "react-native";
+import { FlatList } from "react-native-gesture-handler";
+import { CardMusica } from "../../components/CardMusica";
+import { StackParamList } from "../../routes/routes";
+import { buscaDetalhesAlbum } from "../../services/api";
+import { styles } from "./styles";
+
+import GoBackIcon from '../../assets/icons/goback.png';
+import { ModalLetra } from "../../components/ModalLetra";
+
+type MusicaScreenProps = NativeStackScreenProps<StackParamList,'Musicas'>;
+
+interface DetalhesAlbumProps {
+    id: number, 
+    idArtista: number, 
+    musicas: MusicaProps[],
+    nomeArtista: string, 
+    titulo: string,
+}
+
+export interface MusicaProps {
+    id: number, 
+    idArtista?: number, 
+    minutos: number, 
+    nomeArtista?: string,
+    titulo: string,
+    idAlbum?: number
+}
+
+
+export function Musicas({route, navigation}: MusicaScreenProps) {
+
+    const [album,setAlbum] = useState<DetalhesAlbumProps>();
+    const [idAlbum, setIdAlbum] = useState<number>(0);
+    const [idMusica, setIdMusica] = useState<number>(0);
+    const [visibilidadeModal, setVisibilidadeModal] = useState<boolean>(false);
+
+    useEffect(()=>{
+        buscaDetalhesAlbum(route.params.indexAlbum).then((res)=>{
+            setAlbum(() =>{
+                let albumTemporario : DetalhesAlbumProps = res.data
+
+                albumTemporario.musicas.map((musica)=>{
+                    musica.idAlbum = albumTemporario.id
+                });
+
+                return albumTemporario
+            })
+        }).catch((err)=>{
+            console.log(err)
+        })
+    },[])
+    
+    return <View style={styles.container}>
+        {album ? <>
+            <View style={styles.containerTitulo}>
+                <View style={styles.containerIconTitulo}>
+                    <TouchableOpacity onPress={()=> navigation.goBack()}>
+                        <Image source={GoBackIcon} style={{width: 20, height: 20}} />
+                    </TouchableOpacity>
+                    <Text style={styles.titulo}>
+                        {album.titulo}
+                    </Text>
+                </View>
+                <Text style={styles.artista}>
+                    {album.nomeArtista}
+                </Text>
+            </View>
+            <FlatList
+                data={album.musicas}
+                renderItem={({item})=>{
+                    return <CardMusica setVisibilidadeModal={setVisibilidadeModal} setIdMusica={setIdMusica} setIdAlbum={setIdAlbum} item={item} navigate={navigation}/>
+                }}
+            />
+            {visibilidadeModal && <ModalLetra
+                idAlbum={idAlbum}
+                idMusica={idMusica}
+                visibilidadeModal={visibilidadeModal}
+                setVisibilidadeModal={setVisibilidadeModal}
+            />}
+        </> : <Text style={styles.titulo}>Carregando...</Text>}
+
+    </View>
+}
