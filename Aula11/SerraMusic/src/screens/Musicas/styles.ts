0009ba6e1771dba83df5942d26e523854a6067b9
FredHSQ <frederico.shere@gmail.com>
2022-08-24 09:21:55 -0300

    Aula 11

---
+++ Aula11/SerraMusic/src/screens/Musicas/styles.ts
@@ -0,0 +1,28 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    container: {
+        paddingBottom: 10,
+        paddingTop: 20,
+        backgroundColor: '#e4e4e4',
+        flex:1,
+    },
+    containerTitulo:{
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginBottom: 10,
+        alignItems: 'flex-end',
+        borderBottomWidth: 2,
+    },
+    containerIconTitulo:{
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    titulo:{
+        fontWeight: 'bold',
+        fontSize: 30
+    },
+    artista:{
+        
+    }
+})
